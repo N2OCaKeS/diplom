4a0e062d3cbcbf8f63ea7fb7edb3981d39081a11
Maksim Filippenko <n2o.cake@gmail.com>
2025-11-15 16:48:03 +0300

    fix

--- N2O-ConteinerGuard/app/main.py
+++ N2O-ConteinerGuard/app/main.py
@@ -4,8 +4,7 @@ from app.api.routers.health import router as health_router
 
 app = FastAPI(title="N2O Container Guard API", version="0.1.0")
 
-# роуты
 app.include_router(health_router, prefix="/api")
 
-# раздача статических отчётов (понадобится позже)
-app.mount("/out", StaticFiles(directory="app/static/out"), name="out")
+# чтобы не падать, если каталога ещё нет, либо создай папки в репо
+app.mount("/out", StaticFiles(directory="app/static/out", check_dir=False), name="out")
